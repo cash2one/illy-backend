cf773296f28a6267275c8a46c2ee7b7f2cc027c2
Frank <kuando@126.com>
2015-07-28 18:10:18 +0800

    fix

--- src/app/api/v1/user.api.js
+++ src/app/api/v1/user.api.js
@@ -8,7 +8,7 @@ var jwt = require('jsonwebtoken');
 var cache = require('../../common/cache');
 var config = require('../../../config/config');
 var models = require('../../models');
-var Visitor = models.Vistor;
+var Visitor = models.Visitor;
 var User = models.Student;
 
 /**
@@ -17,7 +17,7 @@ var User = models.Student;
  *
  */
 exports.profile = function *() {
-  this.body = this.request.user;
+    this.body = this.request.user;
 };
 
 
@@ -27,22 +27,22 @@ exports.profile = function *() {
  *
  */
 exports.bind = function *() {
-  var formUser = this.request.body;
-  var openid = formUser.openid;
-  if (!openid) {
-    this.throw(400, '缺少微信绑定信息');
-  }
-  if (!formUser || !formUser.username || !formUser.password) {
-    this.throw(400, '用户名或者密码不能为空');
-  }
-  var user = yield User.findOne({username: formUser.username, state: 0}, 'username password schoolId').exec();
-  if (!user || !user.authenticate(formUser.password)) {
-    this.throw(400, '用户名或密码错误');
-  }
-  yield User.update({_id: user._id}, {$addToSet: {openids: openid}}).exec();
-  var token = jwt.sign({openid: openid, _id: user._id, schoolId: user.schoolId}, config.jwt.secret);
-  yield cache.set('openid:' + openid, token, 3600 * 24); //添加缓存24小时
-  this.body = token;
+    var formUser = this.request.body;
+    var openid = formUser.openid;
+    if (!openid) {
+        this.throw(400, '缺少微信绑定信息');
+    }
+    if (!formUser || !formUser.username || !formUser.password) {
+        this.throw(400, '用户名或者密码不能为空');
+    }
+    var user = yield User.findOne({username: formUser.username, state: 0}, 'username password schoolId').exec();
+    if (!user || !user.authenticate(formUser.password)) {
+        this.throw(400, '用户名或密码错误');
+    }
+    yield User.update({_id: user._id}, {$addToSet: {openids: openid}}).exec();
+    var token = jwt.sign({openid: openid, _id: user._id, schoolId: user.schoolId}, config.jwt.secret);
+    yield cache.set('openid:' + openid, token, 3600 * 24); //添加缓存24小时
+    this.body = token;
 
 };
 
@@ -53,21 +53,21 @@ exports.bind = function *() {
  *
  */
 exports.auth = function *() {
-  var weixinToken = this.request.weixinToken;
-  var openid = weixinToken.openid;
-  var token = yield cache.get('openid:' + openid);
-  if (token) {
-    this.body = token;
-  } else {
-    var user = yield User.findOne({openids: openid}, 'schoolId').exec();
-    if (!user) {
-      user = yield Visitor.findOne({openid: openid}, 'schoolId').exec();
+    var weixinToken = this.request.weixinToken;
+    var openid = weixinToken.openid;
+    var token = yield cache.get('openid:' + openid);
+    if (token) {
+        this.body = token;
+    } else {
+        var user = yield User.findOne({openids: openid}, 'schoolId').exec();
+        if (!user) {
+            user = yield Visitor.findOne({openid: openid}, 'schoolId').exec();
+        }
+        if (!user) {
+            this.throw(401, openid);
+        }
+        token = jwt.sign({openid: openid, _id: user._id, schoolId: user.schoolId}, config.jwt.secret);
+        yield cache.set('openid:' + openid, token, 3600 * 24);
+        this.body = token;
     }
-    if (!user) {
-      this.throw(401, openid);
-    }
-    token = jwt.sign({openid: openid, _id: user._id, schoolId: user.schoolId}, config.jwt.secret);
-    yield cache.set('openid:' + openid, token, 3600 * 24);
-    this.body = token;
-  }
 };
--- src/app/models/user/student.model.js
+++ src/app/models/user/student.model.js
@@ -9,128 +9,121 @@ var ObjectId = Schema.Types.ObjectId;
  * @type {Schema}
  */
 var studentSchema = new Schema({
-  // 用户名,用户登陆系统所用的名字
-  username: {
-    type: String,
-    trim: true,
-    index: true,
-    unique: true,
-    required: '学号不能为空'
-  },
-  //密码
-  password: {
-    type: String,
-    required: '密码不能为空'
-  },
-  // 姓名
-  displayName: {
-    type: String,
-    trim: true
-  },
+    // 用户名,用户登陆系统所用的名字
+    username: {
+        type: String,
+        trim: true,
+        index: true,
+        unique: true,
+        required: '学号不能为空'
+    },
+    //密码
+    password: {
+        type: String,
+        required: '密码不能为空'
+    },
+    // 姓名
+    displayName: {
+        type: String,
+        trim: true
+    },
 
-  // 性别
-  gender: {
-    type: String,
-    trim: true
-    //enum: ['男', '女'] //(0:女 1:男)
-  },
+    // 性别
+    gender: {
+        type: String,
+        trim: true
+        //enum: ['男', '女'] //(0:女 1:男)
+    },
 
-  // 头像
-  avatar: {
-    type: String,
-    trim: true
-  },
-  // 生日
-  birthday: {
-    type: Date
-  },
-  // 联系方式
-  phone: {
-    type: String,
-    trim: true
-  },
-  // 家长
-  parent: {
-    type: String,
-    trim: true
+    // 头像
+    avatar: {
+        type: String,
+        trim: true
+    },
+    // 生日
+    birthday: {
+        type: Date
+    },
+    // 联系方式
+    phone: {
+        type: String,
+        trim: true
+    },
+    // 家长
+    parent: {
+        type: String,
+        trim: true
 
-  },
-  // 积分
-  score: {
-    type: Number,
-    default: 0
-  },
-  // 就读公办学校
-  //onSchool: {
-  //    schoolName: {
-  //        type: String,
-  //        trim: true
-  //    },
-  //    onGrade: {
-  //        type: String,
-  //        trim: true
-  //    }
-  //},
-  // 就读公办学校
-  onSchool: {
-    type: String
-  },
-  grade: {
-    type: String
-  },
-  //备注
-  remark: {
-    type: String,
-    trim: true
-  },
-  // 班级
-  classes: [{
-    type: ObjectId,
-    ref: 'Class'
-  }],
-  //状态
-  state: {
-    type: Number,
-    enum: [0, 1], //(0:可用 1:禁用 2:无班级)
-    default: 0
-  },
-  // 完成作业数量
-  finishedHomeworkCount: {
-    type: Number,
-    default: 0
-  },
-  // 完成预习数量
-  finishedPreviewsCount: {
-    type: Number,
-    default: 0
-  },
-  // 创建时间
-  createdTime: {
-    type: Date,
-    default: Date.now
-  },
-  // 所在培训学校编号
-  schoolId: {
-    type: ObjectId
-  }
+    },
+    // 积分
+    score: {
+        type: Number,
+        default: 0
+    },
+    // 就读公办学校
+    onSchool: {
+        type: String
+    },
+    grade: {
+        type: String
+    },
+    //备注
+    remark: {
+        type: String,
+        trim: true
+    },
+    // 班级
+    classes: [{
+        type: ObjectId,
+        ref: 'Class'
+    }],
+    //状态
+    state: {
+        type: Number,
+        enum: [0, 1], //(0:可用 1:禁用 2:无班级)
+        default: 0
+    },
+    openids: {
+        type: [String],
+        index: true
+    },
+    // 完成作业数量
+    finishedHomeworkCount: {
+        type: Number,
+        default: 0
+    },
+    // 完成预习数量
+    finishedPreviewsCount: {
+        type: Number,
+        default: 0
+    },
+    // 创建时间
+    createdTime: {
+        type: Date,
+        default: Date.now
+    },
+    // 所在培训学校编号
+    schoolId: {
+        type: ObjectId
+    }
 });
 
 
 studentSchema.virtual('role').get(function () {
-  return roles.STUDENT;
+    return roles.STUDENT;
 });
 
 studentSchema.methods.authenticate = function (password) {
-  return this.password === password;
+    return this.password === password;
 };
 
 studentSchema.methods.isDisabled = function () {
-  return this.state === 1;
+    return this.state === 1;
 };
 
 studentSchema.index({schoolId: 1, state: 1});
 studentSchema.index({classes: 1});
 module.exports = {
-  Student: mongoose.model('Student', studentSchema)
+    Student: mongoose.model('Student', studentSchema)
 };
 

39addee2149dcd379d639edf2b1aa93bcd8beeaa
Frank <kuando@126.com>
2015-09-17 23:55:50 +0800

    add log

--- src/app/weixin/event/handlers/click.event.js
+++ src/app/weixin/event/handlers/click.event.js
@@ -8,27 +8,24 @@
 'use strict';
 var co = require('co');
 var _ = require('lodash');
-var format = require('string-template');
 var models = require('../../../models');
-var messageTemplate = '<xml>' +
-    '<ToUserName><![CDATA[{to}]]></ToUserName>' +
-    '<FromUserName><![CDATA[{from}]]></FromUserName>' +
-    '<CreateTime>{createTime}</CreateTime>' +
-    '<MsgType><![CDATA[text]]></MsgType>' +
-    '<Content><![CDATA[{content}]]></Content>' +
-    '</xml>';
+
 /**
  * 在线答疑按钮点击事件
  */
 var qOnline = co.wrap(function *(msg) {
+    let replayTo = msg.FromUserName;
+    let replayFrom = msg.ToUserName;
+    let createTime = new Date().getTime();
+    let replay = content => `<xml>
+            <ToUserName><![CDATA[${replayTo}]]></ToUserName>
+            <FromUserName><![CDATA[${replayFrom}]]></FromUserName>
+            <CreateTime>${createTime}</CreateTime>
+            <MsgType><![CDATA[text]]></MsgType>
+            <Content><![CDATA[${content}]]></Content>
+        </xml>`;
 
-    return format(messageTemplate,
-        {
-            to: msg.FromUserName,
-            from: msg.ToUserName,
-            createTime: new Date().getTime(),
-            content: '在线答疑'
-        });
+    return replay('在线答疑')
 
 });
 
@@ -38,5 +35,6 @@ module.exports = exports = {
         if (key && key === 'q_online') {
             return qOnline(msg);
         }
+        return "";
     }
 };

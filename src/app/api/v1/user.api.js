1458a51344c84ff475b83681c1b0f5f733c0f4c6
Frank <kuando@126.com>
2016-01-13 00:32:44 +0800

    无openid验证

--- src/app/api/v1/user.api.js
+++ src/app/api/v1/user.api.js
@@ -5,7 +5,6 @@
 'use strict';
 
 var jwt = require('jsonwebtoken');
-var cache = require('../../common/cache');
 var config = require('../../../config/config');
 var models = require('../../models');
 var qn = require('../../qiniu');
@@ -25,25 +24,26 @@ var userApi = {
      * 绑定用户微信
      */
     bind: function *() {
-        var formUser = this.request.body;
-        var openid = formUser.openid;
-        var username = formUser.username;
-        var password = formUser.password;
-        if (!openid) {
-            this.throw(400, '缺少微信绑定信息');
-        }
+        let formUser = this.request.body;
+        let openid = formUser.openid;
+        let username = formUser.username;
+        let password = formUser.password;
         if (!username || !password) {
             this.throw(400, '用户名或者密码不能为空');
         }
-        var user = yield User.findOne({username: username, state: 0}, 'username password schoolId').exec();
+        let user = yield User.findOne({username: username, state: 0})
+            .select('username password schoolId').exec();
+
         if (!user || !user.authenticate(password)) {
             this.throw(400, '用户名或密码错误');
         }
-        yield User.update({_id: user._id}, {$addToSet: {openids: openid}}).exec();
-        var token = jwt.sign({openid: openid, _id: user._id, schoolId: user.schoolId}, config.jwt.secret);
-        yield cache.set('token:user:' + openid, token, 3600 * 24); //添加缓存24小时
-        this.body = token;
-
+        let signInfo = {_id: user._id, schoolId: user.schoolId};
+        if (openid) {
+            signInfo.openid = openid;
+            user.openids.addToSet(openid);
+            yield user.save();
+        }
+        this.body = jwt.sign(signInfo, config.jwt.secret);
     },
 
 
@@ -51,42 +51,36 @@ var userApi = {
      * 验证用户是否绑定微信
      */
     auth: function *() {
-        var openid = this.request.openid;
-        var type = this.request.query.authType;
-        var schoolNum = this.request.query.school;
-        var cacheKey;
+        let openid = this.request.openid;
+        let type = this.query.authType;
+        let schoolNum = this.query.school;
+        let token;
+        let user;
+        //如果存在schoolNum说明是查看微网站
+        if (schoolNum) {
+            let school = yield School.findOne({username: schoolNum}).select('_id').lean().exec();
+            if (!school) {
+                this.throw(400, 'School not exist : ', schoolNum);
+            }
+            return this.body = jwt.sign({schoolId: school._id.toString()});
+        }
+
+        if (!openid) {
+            this.throw(400, 'openid not found');
+        }
+
         if (type && type === 'visitor') {
-            cacheKey = 'token:visitor:' + openid;
-        } else {
-            cacheKey = 'token:user:' + openid;
+            user = yield Visitor.findOne({openid: openid}, 'schoolId').exec();
         }
-        // TODO 添加超时机制
-        var token = yield cache.get(cacheKey);
-        if (token) {
-            this.body = token;
-        } else {
-            var user;
-            if (type && type === 'visitor') {
-                user = yield Visitor.findOne({openid: openid}, 'schoolId').exec();
-            }
-            if (!user) {
-                user = yield User.findOne({openids: openid}, 'schoolId').exec();
-            }
-            //用于订阅号微网站查看
-            if (!user && schoolNum && schoolNum !== '') {
-                let school = yield School.findOne({username: schoolNum}).select('_id').lean().exec();
-                if (!school) {
-                    this.throw(400, 'School not exist : ', schoolNum);
-                }
-                user = {openid: '', _id: '', schoolId: school._id.toString()};
-            }
-            if (!user || user === null) {
-                this.throw(401, 'User not found : openid [ ' + openid + ' ]');
-            }
-            token = jwt.sign({openid: openid, _id: user._id, schoolId: user.schoolId}, config.jwt.secret);
-            yield cache.set(cacheKey, token, 3600 * 24);
-            this.body = token;
+        if (!user || user === null) {
+            user = yield User.findOne({openids: openid}, 'schoolId').exec();
         }
+
+        if (!user || user === null) {
+            this.throw(401, 'User not found : openid [ ' + openid + ' ]');
+        }
+        token = jwt.sign({openid: openid, _id: user._id, schoolId: user.schoolId}, config.jwt.secret);
+        this.body = token;
     },
 
     /**
@@ -124,7 +118,9 @@ var userApi = {
         var userId = user._id;
         var openid = user.openid;
         // 解除用户绑定
-        yield cache.delete('token:user:' + openid);
+        if (!openid) {
+            return this.body = "";
+        }
         yield User.update({_id: userId}, {$pull: {openids: openid}}).exec();
         this.body = openid;
     }
